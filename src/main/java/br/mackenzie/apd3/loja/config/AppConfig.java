702753fa31b5da6e01e9eb7c0b3fe6a1f5242e02
bweninger <bweninger@outlook.com>
2015-03-31 21:35:02 -0300

    Primeiro commit -> Configurações iniciais do projeto

---
+++ src/main/java/br/mackenzie/apd3/loja/config/AppConfig.java
@@ -0,0 +1,10 @@
+package br.mackenzie.apd3.loja.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages="br.mackenzie.apd3.loja")
+public class AppConfig {
+
+}
---
+++ src/main/java/br/mackenzie/apd3/loja/config/WebMvnConfig.java
@@ -0,0 +1,11 @@
+package br.mackenzie.apd3.loja.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+public class WebMvnConfig extends WebMvcConfigurerAdapter{
+
+}
