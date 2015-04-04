bb5f057be83fc199d952aea702b9a3b1dc8c8fdb
Julio Cesar Maciel <jmaciel33@gmail.com>
2015-04-03 22:39:36 -0300

    Correção
    GenericDAO

--- src/main/java/br/mackenzie/apd3/loja/dao/GenericDAO.java
+++ src/main/java/br/mackenzie/apd3/loja/dao/GenericDAO.java
@@ -5,17 +5,22 @@ import java.util.List;
 
 /**
  * Generic Dao
+ * 
  * @author Maciel
  *
  * @param <T>
  * @param <Type>
  */
-public interface GenericDAO<T,Type extends Serializable> {
+public interface GenericDAO<T, Type extends Serializable> {
 
 	public T buscarPorChave(T chave);
+
 	public List<T> listar();
+
 	public void incluir();
+
 	public void excluir();
+
 	public void atualizar();
 
 }
